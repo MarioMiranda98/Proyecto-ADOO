8dcec92620aebc7b20135743186587001b4fbd88
MarioMiranda98 <farolin95@msn.com>
2019-11-06 18:55:59 -0600

    first commit

---
+++ ProyectoADOO/src/reclusos/Conexion.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package reclusos;
+
+import java.sql.*;
+
+/**
+ *
+ * @author FAROL
+ */
+public class Conexion {
+    public Conexion() {
+        conexion = null;
+    }
+    
+    public Connection dameConexion() {
+        try {
+            conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/adooreclusos?serverTimezone=UTC&SSL=false", "root", "root");
+            System.out.println("Exito!!");
+        }catch(Exception e) { e.getMessage(); }
+        
+        return conexion;
+    }
+    
+    public Connection conexion;
+}
